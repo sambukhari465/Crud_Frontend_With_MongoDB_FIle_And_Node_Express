0cd444a4eb1ae69d1557dcda43228012a444d702
Syed Sami-Ul-Haider <sambukhari465@gmail.com>
2023-12-06 17:44:35 +0500

    Again Code Push

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+
 function Update() {
   const location = useLocation();
   const { data1 } = location.state;
@@ -11,15 +12,30 @@ function Update() {
     email: data1.email,
     password: data1.password,
     age: data1.age,
-    image: data1.image,
+    image: null, 
   });
+
   const handleChange = (e) => {
     setUpdateUser({ ...updateUser, [e.target.name]: e.target.value });
   };
+
+  const handleImageChange = (e) => {
+    setUpdateUser({ ...updateUser, image: e.target.files[0] });
+  };
+  
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("name", updateUser.name);
+    formData.append("email", updateUser.email);
+    formData.append("password", updateUser.password);
+    formData.append("age", updateUser.age);
+    formData.append("image", updateUser.image);
+
     axios
-      .put(`http://localhost:4006/update/${updateUser.id}`, updateUser)
+      .put(`http://localhost:4006/update/${updateUser.id}`, formData)
       .then((res) => {
         console.log("res", res);
         navigate("/");
@@ -28,13 +44,14 @@ function Update() {
         console.log(err);
       });
   };
+
   return (
     <div className="main">
       <div className="form">
         <div className="detail">
           <h1>Update CRUD Form</h1>
           <form encType="multipart/form-data">
-            <label for="Name">Full Name:</label>
+            <label htmlFor="Name">Full Name:</label>
             <input
               type="text"
               name="name"
@@ -42,7 +59,7 @@ function Update() {
               onChange={handleChange}
               placeholder="Enter your name"
             />
-            <label for="Email">E-mail:</label>
+            <label htmlFor="Email">E-mail:</label>
             <input
               type="text"
               name="email"
@@ -50,7 +67,7 @@ function Update() {
               onChange={handleChange}
               placeholder="Enter your Email"
             />
-            <label for="Password">Password:</label>
+            <label htmlFor="Password">Password:</label>
             <input
               type="text"
               name="password"
@@ -58,13 +75,20 @@ function Update() {
               onChange={handleChange}
               placeholder="Enter your password"
             />
-            <label for="Number">Age:</label>
+            <label htmlFor="Number">Age:</label>
             <input
               type="text"
               name="age"
               value={updateUser.age}
               onChange={handleChange}
               placeholder="Enter your age"
+            />
+            <label htmlFor="File">File:</label>
+            <input
+              type="file"
+              name="image"
+              accept="image/*"
+              onChange={handleImageChange}
             />
             <button type="submit" onClick={handleSubmit}>
               Submit
